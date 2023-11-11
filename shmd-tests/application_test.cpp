348c3ed7c0bbcc9386535d54536552df2d9e6ee2
dmitriy <linev@programmer.net>
2023-11-11 12:14:26 +0200

    Add test for a default sleep function

--- shmd-tests/application_test.cpp
+++ shmd-tests/application_test.cpp
@@ -2,6 +2,8 @@
 
 #include "application.h"
 
+#include <numeric>
+
 TEST(ApplicationTest, run) {
     using namespace std::chrono;
 
@@ -35,4 +37,23 @@ TEST(ApplicationTest, run) {
     EXPECT_EQ(sleepDurations[1], milliseconds(400));
     EXPECT_EQ(sleepDurations[2], milliseconds(300));
     EXPECT_EQ(sleepDurations[3], milliseconds(200));
+}
+
+TEST(ApplicationTest, sleep) {
+    using namespace std::chrono;
+    const int n = 50;
+    std::vector<int> sleeps;
+    sleeps.reserve(n);
+    for (int i = 0; i < n; i++) {
+        auto start = high_resolution_clock::now();
+        defaultSleepFunction(120);
+        auto end = high_resolution_clock::now();
+        int ms = duration_cast<milliseconds>(end - start).count();
+        sleeps.push_back(ms);
+    }
+    double average = static_cast<double>(
+                         std::accumulate(sleeps.begin(), sleeps.end(), 0)) /
+                     sleeps.size();
+
+    ASSERT_NEAR(average, 120, 1);
 }
